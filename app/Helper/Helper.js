c30974f3675c9ca0d74b5aa1732e541550d2d267
Martins <aloba.martins@gmail.com>
2018-12-24 14:38:38 +0100

    modify helper functions

--- app/Helper/Helper.js
+++ app/Helper/Helper.js
@@ -5,30 +5,27 @@ import dotenv from 'dotenv';
 dotenv.config();
 const SECRET = process.env.SECRET;
 
-const helper = {
-  hashPassword(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-  },
+class Helper {
+    static hashPassword(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+  };
 
   // To compare registered and signin password
-  comparePassword(hashPassword, password) {
+  static comparePassword(hashPassword, password){
     return bcrypt.compareSync(password, hashPassword);
-  },
-
-  // To validate email
-  isValidEmail(email) {
-    return /\S+@\S+\.\S/.test(email);
-  },
+  };
 
   // Token generation
-  generateToken(id) {
+  static generateToken(id) {
     const token = jwt.sign({
       userId: id
     },
       SECRET, { expiresIn: '2d' }
     );
     return token;
-  }
+  };
 }
 
-export default helper;
+
+
+export default Helper;
